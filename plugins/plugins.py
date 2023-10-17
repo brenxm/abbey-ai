e1b7a04fcbd8ea61f7e17d4b844f20a7c4223573
Bryan Mina <brymina18@yahoo.com>
2023-10-17 09:47:02 -0500

    further work

--- plugins/plugins.py
+++ plugins/plugins.py
@@ -8,20 +8,35 @@ class Plugins():
 
     def load_plugins(self, paths):
         for path in paths:
-
-            for file in os.listdir(path):
+            path_contents = [file for file in os.listdir(path) if not file.endswith('__') and not file.startswith('__')] # Removing systems directory
+            for file in path_contents:
                 temp_path = os.path.join(path, file)
-
+                
                 if os.path.isdir(temp_path):
-                    for filename in os.listdir(temp_path):  
-                        if filename.endswith('_module.py'):
-                            module_name = filename[:-3]
-                            sys.path.append(temp_path)
-                            module = importlib.import_module(module_name)
+                    self.load_plugin_from_dir(temp_path)
 
-                            if hasattr(module, 'register'):
-                                module_instance = module.register()
-                                self.loaded_plugins.append(module_instance)
+            print("All plugins are checked.")
+
+
+    def load_plugin_from_dir(self, path):
+        for filename in os.path.listdir(path):
+            if filename.endswitch('_module.py'):
+                module_name = filename[:-3]
+                sys.path.append(path)
+                module = importlib.import_module(module_name)
+
+                if hasattr(module, 'register'):
+                    module_instance = module.register()
+                    self.loaded_plugins.append(module_instance)
+                    return
+                
+                else:
+                    print('A module exist but has no "register" attribute/method implemented.')
+                    return
+        
+        print(f"A directory exist but has no module script")
+
+                                
 
 
 if __name__ == "__main__":
